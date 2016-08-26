f9b071831414bef203d8331a80055fe89807a434
sharadgaikwad <expert.sharad@gmail.com>
2016-08-26 15:25:43 +0530

    Com

---
+++ MySite/MySite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MySite.Global" Language="C#" %>
---
+++ MySite/MySite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySite.Startup))]
+namespace MySite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
